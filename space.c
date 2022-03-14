9742df57032cb682bcac39bb276b6312cb80d571
jobnah <4412@holbertonschool.com>
2022-03-14 14:27:02 +0100

    _printf

--- space.c
+++ space.c
@@ -1,10 +1,17 @@
 #include "main.h"
 
+/**
+ * space - returns the index of the last space
+ * @format: string to check for spqces
+ * @f_i: index of 1st space
+ * Return: int, index of last space
+ */
+
 int space(const char *format, int f_i)
 {
-    while (format[f_i] == ' ')
-    {
-        f_i++;
-    }
-    return (f_i - 1);
+	while (format[f_i] == ' ')
+	{
+		f_i++;
+	}
+	return (f_i - 1);
 }
