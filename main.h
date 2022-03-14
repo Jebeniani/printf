ad9c5b7ee65bccf72b080d9ddcf7774382e1006b
ahmed0097 <ahmed.zghal1233@gmail.com>
2022-03-14 12:43:37 +0000

    _printf

--- main.h
+++ main.h
@@ -6,27 +6,25 @@
 #include <stdarg.h>
 #include <unistd.h>
 
+int print_decimal(va_list args);
+int print_string(va_list args);
+int print_integer(va_list args);
+int print_char(va_list args);
+int print_percent(va_list args);
+int space(const char *format, int f_i);
+int _printf(const char *format, ...);
+int _putchar(char c);
+int (*get_spec_func(char spec))(va_list);
+
 /**
 * struct format - Struct spec_t.
 * @sym: symbol.
 * @f: function.
 */
-
 typedef struct format
 {
 	char *sym;
 	int (*f)(va_list);
 } spec_t;
-
-int space(const char *format, int f_i);
-int _printf(const char *format, ...);
-int _putchar(char c);
-int(*get_spec_func(char spec))(va_list);
-
-int print_char(va_list args);
-int print_percent(va_list args);
-int print_integer(va_list args);
-int print_decimal(va_list args);
-int print_string(va_list args);
 
 #endif
