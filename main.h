7b71dd482ae03ab4bb1ad92b61b9f3cc45f76002
jobnah <4412@holbertonschool.com>
2022-03-14 14:32:52 +0100

    _printf

---
+++ main.h
@@ -0,0 +1,30 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <unistd.h>
+
+int print_decimal(va_list args);
+int print_string(va_list args);
+int print_integer(va_list args);
+int print_char(va_list args);
+int print_percent(va_list args);
+int space(const char *format, int f_i);
+int _printf(const char *format, ...);
+int _putchar(char c);
+int (*get_spec_func(char spec))(va_list);
+
+/**
+ * struct format - Struct spec_t.
+ * @sym: symbol.
+ * @f: function.
+ */
+typedef struct format
+{
+	char *sym;
+	int (*f)(va_list);
+} spec_t;
+
+#endif
---
+++ string_assistance.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_string - prints argument of type char *
+ * @args: argument to print
+ *
+ * Return: number of characters printed
+ */
+
+int print_string(va_list args)
+{
+	int count = 0;
+	char *string = va_arg(args, char *);
+
+
+	if (string == NULL)
+	{
+		write(1, "(null)", 6);
+		return (6);
+	}
+
+	for (; string[count] != '\0'; count++)
+		_putchar(string[count]);
+
+	return (count);
+}
